b09ddffddccf6fd90bc4ed7980e09a371ed6836c
Dan Brooks <danger.dan_@hotmail.co.uk>
2015-07-14 22:59:39 +0100

    Add rotation code

--- src/graphics/Point.js
+++ src/graphics/Point.js
@@ -1,6 +1,10 @@
 
 var _ = require('lodash');
 
+function negativeZero(n) {
+  return (n == 0 && (1/n) !== Number.POSITIVE_INFINITY);;
+};
+
 var Point = function(x, y) {
   this.x = x;
   this.y = y;
@@ -32,6 +36,60 @@ Point.prototype.invert = function(){
   }
 
   return this;
+};
+
+Point.prototype.rotate = function (axis, degrees) {
+
+  if (_.isUndefined(degrees)) {
+    if (!_.isNumber(axis)) {
+      throw new Error('Invalid arguments passed to point.rotate');
+    }
+
+    degrees = axis;
+    axis = Factory(0, 0);
+  }
+
+  if (!_.isNumber(degrees)) {
+    throw new Error('Invalid arguments passed to point.rotate');
+  }
+
+  if (_.isArray(axis)) {
+    axis = Factory(axis);
+  }
+
+  if (false === axis instanceof Point) {
+    throw new Error('Invalid arguments passed to point.rotate');
+  }
+
+  var radians = degrees * (Math.PI / 180);
+
+  var cos = Math.cos(radians);
+  var sin = Math.sin(radians);
+
+  var transform = {};
+  transform.x = (this.x - axis.x);
+  transform.y = (this.y - axis.y);
+
+  var rotate = {
+    x: transform.x * cos - transform.y * sin,
+    y: transform.x * sin + transform.y * cos
+  };
+
+  this.x = Number( (rotate.x + axis.x).toFixed(12) );
+  this.y = Number( (rotate.y + axis.y).toFixed(12) );
+  this.clean();
+};
+
+Point.prototype.clean = function() {
+
+  if (negativeZero(this.x)) {
+    this.x = 0;
+  }
+
+  if (negativeZero(this.y)) {
+    this.y = 0;
+  }
+
 };
 
 var Factory = function(x, y){
