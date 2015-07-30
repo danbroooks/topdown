46bbedf7006b770c9b7f826560dbf2ee7e90cc34
Dan Brooks <dmbrooks@live.co.uk>
2015-07-30 21:33:57 +0100

    Add code to make tests pass

--- src/util/Collection.js
+++ src/util/Collection.js
@@ -1,6 +1,12 @@
 
 var Collection = function(){
+  this.items = [];
+  this.length = 0;
+};
 
+Collection.prototype.add = function(obj) {
+  this.items.push(obj);
+  this.length = this.items.length;
 };
 
 var Factory = function(){
