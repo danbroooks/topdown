a81448219a8322becf75b43248cc1c250e9959de
Dan Brooks <dmbrooks@live.co.uk>
2015-07-30 22:04:06 +0100

    Add failing tests for Collection filter param

--- spec/util/CollectionSpec.js
+++ spec/util/CollectionSpec.js
@@ -1,17 +1,37 @@
 describe("Collection", function() {
 
-  var Collection = require('../../src/util/Collection');
+  var Module = require('../../src/util/Collection');
 
   describe("Factory", function () {
+
+    var Factory = Module;
+    var Constructor = Module.Constructor;
+
     it("should return new instance", function () {
-      expect(Collection() instanceof Collection.Constructor).toBeTruthy();
+      expect(Factory() instanceof Constructor).toBeTruthy();
+    });
+
+    it("should take a filter argument, defaulting to constant true if nothing passed", function(){
+      var c;
+
+      c = Factory();
+      expect(c.filter).toEqual(Constructor.DefaultFilter);
+
+      function stringFilter(val){
+        return typeof val == 'string';
+      }
+
+      c = Factory(stringFilter);
+      expect(c.filter).toEqual(stringFilter);
     });
   });
 
   describe(".add(obj)", function(){
 
+    var Collection = Module.Constructor;
+
     it("should add items to the list", function() {
-      var c = Collection();
+      var c = new Collection();
       c.add(12);
       c.add('hello');
       expect(c.items[0]).toEqual(12);
@@ -19,14 +39,11 @@ describe("Collection", function() {
     });
 
     it("should keep track of the number of items in the collection", function(){
-
-      var c = Collection();
+      var c = new Collection();
       expect(c.length).toEqual(0);
       c.add({});
       expect(c.length).toEqual(1);
-
     });
-    
   });
 
 });
