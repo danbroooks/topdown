e489335880bca5733c540cb2f9abf5b7ab86dee3
Dan Brooks <dmbrooks@live.co.uk>
2015-07-31 00:42:39 +0100

    Add failing tests for Server

--- spec/server/ServerSpec.js
+++ spec/server/ServerSpec.js
@@ -1,10 +1,53 @@
-describe("Server", function() {
 
-  var Server = require('../../src/server/Server');
+var sinon = require('sinon');
+var proxyquire = require('proxyquire');
+
+var httpMock = {};
+
+var Server = proxyquire('../../src/server/Server', {
+  'http': httpMock
+});
+
+describe("Server", function() {
 
   describe("Factory", function () {
     it("should return new instance", function () {
       expect(Server() instanceof Server.Constructor).toBeTruthy();
+    });
+  });
+
+  describe(".Listen static method", function(){
+    it("should listen on the port passed", function(){
+      var listen = sinon.stub();
+      var createServer = sinon.stub();
+
+      createServer.returns({
+        listen: listen
+      });
+
+      httpMock.createServer = createServer;
+
+      Server.Listen(8080);
+
+      expect(listen.calledWith(8080)).toBeTruthy();
+    });
+  });
+
+  describe(".listen(port)", function(){
+    it("should ", function(){
+      var listen = sinon.stub();
+      var createServer = sinon.stub();
+
+      createServer.returns({
+        listen: listen
+      });
+
+      httpMock.createServer = createServer;
+
+      var s = Server(88);
+      s.listen();
+
+      expect(listen.calledWith(88)).toBeTruthy();
     });
   });
 
