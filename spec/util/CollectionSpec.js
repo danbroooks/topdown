79ef518f33bd72183500a33d713b03a2ccfed0cd
Dan Brooks <dmbrooks@live.co.uk>
2015-07-30 21:32:49 +0100

    Add failing tests for Collection

--- spec/util/CollectionSpec.js
+++ spec/util/CollectionSpec.js
@@ -8,4 +8,25 @@ describe("Collection", function() {
     });
   });
 
+  describe(".add(obj)", function(){
+
+    it("should add items to the list", function() {
+      var c = Collection();
+      c.add(12);
+      c.add('hello');
+      expect(c.items[0]).toEqual(12);
+      expect(c.items[1]).toEqual('hello');
+    });
+
+    it("should keep track of the number of items in the collection", function(){
+
+      var c = Collection();
+      expect(c.length).toEqual(0);
+      c.add({});
+      expect(c.length).toEqual(1);
+
+    });
+    
+  });
+
 });
