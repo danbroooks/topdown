5f9493769b9b6c8a2e5d80b833978119fb5f12dc
Dan Brooks <dmbrooks@live.co.uk>
2015-07-31 00:59:39 +0100

    Add jasmine-spec-reporter

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,11 @@
 
 var gulp = require('gulp');
 var jasmine = require('gulp-jasmine');
+var SpecReporter  = require('jasmine-spec-reporter');
 
 gulp.task('jasmine', function () {
   return gulp.src('spec/**/*.js')
-    .pipe(jasmine());
+    .pipe(jasmine({
+      reporter: new SpecReporter()
+    }));
 });
