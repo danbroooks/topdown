ff40c990d5c32c486dc18993c2279a466a2f11db
Dan Brooks <dmbrooks@live.co.uk>
2015-08-05 22:58:44 +0100

    Add test for Connection.Collection

--- spec/server/ConnectionSpec.js
+++ spec/server/ConnectionSpec.js
@@ -3,9 +3,17 @@ describe("Connection", function() {
   var Connection = require('../../src/server/Connection');
 
   describe("Factory", function () {
+
     it("should return new instance", function () {
       expect(Connection() instanceof Connection.Constructor).toBeTruthy();
     });
-  });
 
+    it("should provide static method for generating collection of connections", function(){
+      var col = Connection.Collection();
+      col.add(Connection());
+      col.add("Connection");
+      expect(col.length).toEqual(1);
+    });
+
+  });
 });
