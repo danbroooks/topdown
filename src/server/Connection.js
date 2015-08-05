e97d87f1a09efff4a1238eb8dd03264c0087fcb1
Dan Brooks <dmbrooks@live.co.uk>
2015-08-05 22:59:06 +0100

    Add code to make tests pass

--- src/server/Connection.js
+++ src/server/Connection.js
@@ -1,10 +1,18 @@
 
+var Collection = require('../util/Collection');
+
 var Connection = function(){
 
 };
 
 var Factory = function(){
   return new Connection();
+};
+
+Factory.Collection = function() {
+  return Collection(function(o){
+    return o instanceof Connection;
+  });
 };
 
 Factory.Constructor = Connection;
