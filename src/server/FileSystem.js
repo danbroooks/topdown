21787bdef06c20f400691f83d75b89697ad443d8
Dan Brooks <dmbrooks@live.co.uk>
2015-08-06 20:55:39 +0100

    Add code to make tests pass

--- src/server/FileSystem.js
+++ src/server/FileSystem.js
@@ -1,7 +1,22 @@
 
 var fs = require('fs');
+var join = require("path").join;
 
 var FileSystem = function(){};
+
+FileSystem.prototype.find = function (path, locations, success, failure) {
+  var self = this;
+  var location = locations.shift();
+  this.exists(join(location, path), function () {
+    success();
+  }, function () {
+    if (locations.length) {
+      self.find(path, locations, success, failure);
+    } else {
+      failure();
+    }
+  });
+};
 
 FileSystem.prototype.exists = function (path, success, failure) {
 
