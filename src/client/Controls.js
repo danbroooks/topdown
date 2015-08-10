8f8d83ea6334dca9780bf0702805fb9846527a81
Dan Brooks <dmbrooks@live.co.uk>
2015-08-10 19:29:24 +0100

    Add code to Controls.js to make tests pass

--- src/client/Controls.js
+++ src/client/Controls.js
@@ -1,7 +1,20 @@
 
 var _ = require('lodash');
 
+var Point = require('../graphics/Point');
+
 var Controls = function(win, doc){
+
+  var mouse = Point(0, 0);
+
+  win.onmousemove = function (e) {
+    mouse = Point(e.offsetX, e.offsetY);
+  };
+
+  this.__defineGetter__('mouse', function(){
+    return mouse;
+  });
+
   doc.oncontextmenu = _.constant(false);
 };
 
