f170b721f1fc19aff7d723ea764a902e4bf9d1f4
Dan Brooks <dmbrooks@live.co.uk>
2015-08-10 20:01:24 +0100

    Write Controls code to make tests pass

--- src/client/Controls.js
+++ src/client/Controls.js
@@ -6,13 +6,26 @@ var Point = require('../graphics/Point');
 var Controls = function(win, doc){
 
   var mouse = Point(0, 0);
+  var keystate = {};
 
   win.onmousemove = function (e) {
     mouse = Point(e.offsetX, e.offsetY);
   };
 
+  win.onkeydown = function (key) {
+    keystate[key.which] = true;
+  };
+
+  win.onkeyup = function (key) {
+    delete keystate[key.which];
+  };
+
   this.__defineGetter__('mouse', function(){
     return mouse;
+  });
+
+  this.__defineGetter__('keystate', function(){
+    return keystate;
   });
 
   doc.oncontextmenu = _.constant(false);
