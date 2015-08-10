9704854ff000a94f9813f1055dd314834a96b11c
Dan Brooks <dmbrooks@live.co.uk>
2015-08-10 19:27:33 +0100

    Add failing tests for mouse tracking

--- spec/client/ControlsSpec.js
+++ spec/client/ControlsSpec.js
@@ -1,6 +1,7 @@
 describe("Controls", function() {
 
   var Controls = require('../../src/client/Controls');
+  var Point = require('../../src/graphics/Point');
   var doc, win;
 
   beforeEach(function(){
@@ -20,6 +21,42 @@ describe("Controls", function() {
     it("should block default context menu from opening", function(){
       Controls(win, doc);
       expect(doc.oncontextmenu()).toEqual(false);
+    });
+  });
+
+  describe("Mouse", function () {
+
+    var c;
+
+    beforeEach(function(){
+      win.onmousemove = function() {};
+      c = Controls(win, doc);
+    });
+
+    it("should track user's mouse position as a point object", function(){
+      expect(c.mouse instanceof Point.Constructor).toBeTruthy();
+    });
+
+    it("should track user's mouse position when it moves", function() {
+      win.onmousemove({
+        offsetX: 20, offsetY: 40
+      });
+      expect(c.mouse.x).toEqual(20);
+      expect(c.mouse.y).toEqual(40);
+    });
+
+    it("should be an immutable property", function() {
+      win.onmousemove({
+        offsetX: 20, offsetY: 40
+      });
+
+      expect(c.mouse.x).toEqual(20);
+      expect(c.mouse.y).toEqual(40);
+
+      c.mouse = Point(50, 100);
+
+      expect(c.mouse.x).toEqual(20);
+      expect(c.mouse.y).toEqual(40);
     });
   });
 
