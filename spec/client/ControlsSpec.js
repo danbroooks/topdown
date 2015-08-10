dbdde488d99602c9624b466f2a821787dde219a2
Dan Brooks <dmbrooks@live.co.uk>
2015-08-10 19:17:47 +0100

    Alter Controls spec to include passing in window object

--- spec/client/ControlsSpec.js
+++ spec/client/ControlsSpec.js
@@ -1,23 +1,24 @@
 describe("Controls", function() {
 
   var Controls = require('../../src/client/Controls');
-  var doc;
+  var doc, win;
 
   beforeEach(function(){
     doc = {
       oncontextmenu: undefined
     };
+    win = {};
   });
 
   describe("Factory", function () {
     it("should return new instance", function () {
-      expect(Controls(doc) instanceof Controls.Constructor).toBeTruthy();
+      expect(Controls(win, doc) instanceof Controls.Constructor).toBeTruthy();
     });
   });
 
   describe("Context Menu", function () {
     it("should block default context menu from opening", function(){
-      Controls(doc);
+      Controls(win, doc);
       expect(doc.oncontextmenu()).toEqual(false);
     });
   });
