97cd0a142fe5506d50f3afad9088713b1c37c4f2
Dan Brooks <dmbrooks@live.co.uk>
2015-08-11 19:10:36 +0100

    Update connectionSpec with tests for .on()

--- spec/server/ConnectionSpec.js
+++ spec/server/ConnectionSpec.js
@@ -1,3 +1,5 @@
+var sinon = require('sinon');
+
 describe("Connection", function() {
 
   var Connection = require('../../src/server/Connection');
@@ -13,6 +15,32 @@ describe("Connection", function() {
       col.add(Connection());
       col.add("Connection");
       expect(col.length).toEqual(1);
+    });
+
+  });
+
+  describe(".on", function(){
+
+    var conn, onconnect;
+    var mock = {};
+
+    beforeEach(function () {
+      mock.on = sinon.stub();
+      conn = Connection(mock);
+      onconnect = function(){};
+      conn.on('connect', onconnect);
+    });
+
+    afterEach(function () {
+      mock.on.reset();
+    });
+
+    it("should forward events socket.on", function () {
+      expect(mock.on.called).toBeTruthy();
+    });
+
+    it("should directly pass arguments to socket.on", function () {
+      expect(mock.on.calledWith('connect', onconnect)).toBeTruthy();
     });
 
   });
