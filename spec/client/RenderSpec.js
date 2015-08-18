1ea22fb1e98c88fbc9e4415d56fc3bf4fa7d37b7
Dan Brooks <dmbrooks@live.co.uk>
2015-08-18 23:26:14 +0100

    Add addLayer method with tests

--- spec/client/RenderSpec.js
+++ spec/client/RenderSpec.js
@@ -1,3 +1,5 @@
+var sinon = require('sinon');
+
 describe("Render", function() {
 
   var Render = require('../../src/client/Render');
@@ -11,6 +13,37 @@ describe("Render", function() {
       var doc = {};
       expect(Render(doc).document).toEqual(doc);
     });
+  });
+
+  describe(".addLayer(name)", function () {
+
+    beforeEach(function () {
+      this.body = {};
+      this.body.appendChild = sinon.stub();
+
+      this.canvas = {};
+
+      this.document = {};
+      this.document.getElementsByTagName = sinon.stub();
+      this.document.getElementsByTagName.returns([this.body]);
+      this.document.createElement = sinon.stub();
+      this.document.createElement.returns(this.canvas);
+    });
+
+    it("should add a new canvas to the body of the document", function () {
+      Render(this.document).addLayer('foreground');
+      expect(this.document.getElementsByTagName.calledOnce).toBeTruthy();
+      expect(this.document.getElementsByTagName.calledWith('body')).toBeTruthy();
+      expect(this.body.appendChild.calledOnce).toBeTruthy();
+      expect(this.body.appendChild.calledWith(this.canvas)).toBeTruthy();
+    });
+
+    it("should generate new canvas object, and assign an ID matching the name received as argument", function () {
+      Render(this.document).addLayer('foreground');
+      expect(this.document.createElement.calledWith('canvas')).toBeTruthy();
+      expect(this.canvas.id).toEqual('foreground');
+    });
+
   });
 
 
--- src/client/Render.js
+++ src/client/Render.js
@@ -3,6 +3,13 @@ var Render = function(doc){
   this.document = doc;
 };
 
+Render.prototype.addLayer = function (name) {
+  var body = this.document.getElementsByTagName('body')[0];
+  var canvas = this.document.createElement('canvas');
+  canvas.id = name;
+  body.appendChild(canvas);
+};
+
 var Factory = function(doc){
   return new Render(doc);
 };
