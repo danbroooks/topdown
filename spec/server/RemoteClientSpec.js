85c459f7d0debecb52235730421a44585317bf93
Dan Brooks <dmbrooks@live.co.uk>
2015-08-19 19:20:10 +0100

    Add keymap transformer to RemoteClient object

--- spec/server/RemoteClientSpec.js
+++ spec/server/RemoteClientSpec.js
@@ -1,3 +1,5 @@
+var _ = require('lodash');
+
 describe("RemoteClient", function() {
 
   var RemoteClient = require('../../src/server/RemoteClient');
@@ -5,6 +7,16 @@ describe("RemoteClient", function() {
   describe("Factory", function () {
     it("should return new instance", function () {
       expect(RemoteClient() instanceof RemoteClient.Constructor).toBeTruthy();
+    });
+  });
+
+  describe(".key(string)", function () {
+    it("should translate string representation of a key into a keycode",  function () {
+      var cl = RemoteClient();
+      var keymap = require('../../src/Keymap.js');
+      _.forOwn(keymap, function (val, key) {
+        expect(cl.key(key)).toEqual(val);
+      });
     });
   });
 
--- src/server/RemoteClient.js
+++ src/server/RemoteClient.js
@@ -1,6 +1,14 @@
 
+var Keymap = require('../Keymap');
+
 var RemoteClient = function () {
 
+};
+
+RemoteClient.prototype.key = function(letter){
+  if (Keymap[letter] !== undefined) {
+    return Keymap[letter];
+  }
 };
 
 var Factory = function () {
