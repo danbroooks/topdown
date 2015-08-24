538904477858568dc4d7e93f682b1a14d2b254ce
Dan Brooks <dmbrooks@live.co.uk>
2015-08-24 20:36:13 +0100

    Add istanbul coverage reporting

--- gulp/jasmine.js
+++ gulp/jasmine.js
@@ -2,12 +2,27 @@
 var gulp = require('gulp');
 var jasmine = require('gulp-jasmine');
 var SpecReporter  = require('jasmine-spec-reporter');
+var istanbul = require('gulp-istanbul');
 
-gulp.task('jasmine', function () {
-  return gulp.src('spec/**/*.js')
+function jasmineTask() {
+  return gulp.src(['spec/**/*.js'])
     .pipe(jasmine({
       reporter: new SpecReporter({
         displayStacktrace: 'summary'
       })
     }));
+}
+
+gulp.task('jasmine', jasmineTask);
+
+gulp.task('istanbul', function () {
+
+  return gulp.src(['src/**/*.js'])
+    .pipe(istanbul())
+    .pipe(istanbul.hookRequire())
+    .on('finish', function () {
+      jasmineTask()
+        .pipe(istanbul.writeReports());
+    });
+
 });
