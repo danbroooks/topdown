9acfe961151c9ad4a3baf81d0bafc3ac3746815b
Dan Brooks <dmbrooks@live.co.uk>
2015-09-07 20:59:24 +0100

    Add getIntersectionPoint method to collision

--- spec/physics/CollisionSpec.js
+++ spec/physics/CollisionSpec.js
@@ -4,25 +4,19 @@ describe("Collision", function () {
   var sinon = require('sinon');
   var proxyquire = require('proxyquire');
   var Vector = require('../../src/graphics/Vector');
-  var Collision;
+  var Collision = proxyquire('../../src/physics/Collision', {
+    'Vector': Vector
+  });
 
   beforeEach(function () {
-    this.VectorMock = Vector;
+    this.stub = sinon.stub(Vector, 'Created');
+  });
 
-    Collision = proxyquire('../../src/physics/Collision', {
-      'Vector': this.VectorMock
-    });
+  afterEach(function () {
+    this.stub.restore();
   });
 
   describe("Factory", function () {
-
-    beforeEach(function () {
-      this.stub = sinon.stub(this.VectorMock, 'Created');
-    });
-
-    afterEach(function () {
-      this.stub.restore();
-    });
 
     it("should throw error when not passed vector objects", function () {
       this.stub.returns(false);
@@ -52,4 +46,46 @@ describe("Collision", function () {
       expect(c.vb).toEqual(vb);
     });
   });
+
+  describe(".getIntersectionPoint", function () {
+
+    beforeEach(function () {
+      this.stub.returns(true);
+    });
+
+    it("should return false if no intersection is found", function () {
+      var va = {
+        from: { x: -1, y: -1 },
+        to: { x: 1, y: -1 }
+      };
+
+      var vb = {
+        from: { x: -1, y: 1 },
+        to: { x: 1, y: 1 },
+      };
+
+      var c = Collision(va, vb);
+      expect(c.getIntersectionPoint()).toEqual(false);
+    });
+
+    it("should work out the intersection point of two vectors", function () {
+      var va = {
+        from: { x: -1, y: 1 },
+        to: { x: 1, y: -1 }
+      };
+
+      var vb = {
+        from: { x: -1, y: -1 },
+        to: { x: 1, y: 1 },
+      };
+
+      var c = Collision(va, vb);
+      var i = c.getIntersectionPoint();
+
+      expect(i.x).toEqual(0);
+      expect(i.y).toEqual(0);
+    });
+
+  });
+
 });
--- src/physics/Collision.js
+++ src/physics/Collision.js
@@ -1,4 +1,5 @@
 var Build = require('../util/Factory').Build;
+var Point = require('../graphics/Point');
 var Vector = require('../graphics/Vector');
 
 var Collision = function () {};
@@ -6,6 +7,33 @@ var Collision = function () {};
 Collision.prototype.va = undefined;
 
 Collision.prototype.vb = undefined;
+
+Collision.prototype.getIntersectionPoint = function () {
+
+  var vectorA = this.va;
+  var vectorB = this.vb;
+
+  var s1_x, s1_y, s2_x, s2_y;
+
+  s1_x = vectorA.to.x - vectorA.from.x;
+  s1_y = vectorA.to.y - vectorA.from.y;
+  s2_x = vectorB.to.x - vectorB.from.x;
+  s2_y = vectorB.to.y - vectorB.from.y;
+
+  var s = (-s1_y * (vectorA.from.x - vectorB.from.x) + s1_x * (vectorA.from.y - vectorB.from.y)) / (-s2_x * s1_y + s1_x * s2_y);
+  var t = (s2_x * (vectorA.from.y - vectorB.from.y) - s2_y * (vectorA.from.x - vectorB.from.x)) / (-s2_x * s1_y + s1_x * s2_y);
+
+  if (s >= 0 && s <= 1 && t >= 0 && t <= 1) {
+    // Collision detected
+    return Point({
+      x: vectorA.from.x + (t * s1_x),
+      y: vectorA.from.y + (t * s1_y)
+    });
+  }
+
+  // No collision
+  return false;
+};
 
 var Factory = Build(Collision, function (va, vb) {
   if (!Vector.Created(va) || !Vector.Created(vb)) {
