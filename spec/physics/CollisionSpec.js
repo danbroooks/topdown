a22eae47a39dd32406bb230ead388994a5292098
Dan Brooks <dmbrooks@live.co.uk>
2015-09-07 20:55:34 +0100

    Ensure only vectors can be passed to collision object

--- spec/physics/CollisionSpec.js
+++ spec/physics/CollisionSpec.js
@@ -1,10 +1,50 @@
 describe("Collision", function () {
 
-  var Collision = require('../../src/physics/Collision');
+  var _ = require('lodash');
+  var sinon = require('sinon');
+  var proxyquire = require('proxyquire');
+  var Vector = require('../../src/graphics/Vector');
+  var Collision;
+
+  beforeEach(function () {
+    this.VectorMock = Vector;
+
+    Collision = proxyquire('../../src/physics/Collision', {
+      'Vector': this.VectorMock
+    });
+  });
 
   describe("Factory", function () {
 
+    beforeEach(function () {
+      this.stub = sinon.stub(this.VectorMock, 'Created');
+    });
+
+    afterEach(function () {
+      this.stub.restore();
+    });
+
+    it("should throw error when not passed vector objects", function () {
+      this.stub.returns(false);
+
+      expect(function () {
+          Collision();
+        })
+        .toThrowError('Invalid arguments passed to Collision, requires two Vectors');
+    });
+
+    it("should not throw error when passed a vector object", function () {
+      this.stub.returns(true);
+
+      expect(function () {
+          Collision();
+        })
+        .not.toThrow();
+    });
+
     it("should attach passed vector objects to collision object", function () {
+      this.stub.returns(true);
+
       var va = {};
       var vb = {};
       var c = Collision(va, vb);
--- src/physics/Collision.js
+++ src/physics/Collision.js
@@ -1,4 +1,5 @@
 var Build = require('../util/Factory').Build;
+var Vector = require('../graphics/Vector');
 
 var Collision = function () {};
 
@@ -7,6 +8,9 @@ Collision.prototype.va = undefined;
 Collision.prototype.vb = undefined;
 
 var Factory = Build(Collision, function (va, vb) {
+  if (!Vector.Created(va) || !Vector.Created(vb)) {
+    throw new Error('Invalid arguments passed to Collision, requires two Vectors');
+  }
 
   return {
     va: va,
