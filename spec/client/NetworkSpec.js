d515640db71faa55f31bd4f3a48e6184b4e191f7
Dan Brooks <dmbrooks@live.co.uk>
2015-09-16 21:31:44 +0100

    Alter Network to not connect on construction, use method

--- spec/client/NetworkSpec.js
+++ spec/client/NetworkSpec.js
@@ -30,36 +30,62 @@ describe("Network", function () {
     it("should return new instance", function () {
       expect(Network() instanceof Network.Constructor).toBeTruthy();
     });
+  });
 
-    it("should return new instance", function () {
-      Network('localhost');
+  describe(".connect(server)", function () {
+    it("should connect to server passed as argument", function () {
+      Network().connect('localhost');
       expect(iomock.connect.calledWith('localhost')).toBeTruthy();
+    });
+
+    it("should be chainable", function () {
+      var n = Network();
+      expect(n.connect('localhost')).toEqual(n);
     });
   });
 
   describe('.on(event, hander)', function () {
 
+    beforeEach(function () {
+      this.network = Network().connect('localhost');
+    });
+
     it("should be chainable", function () {
-      var n = Network();
-      expect(n.on(null, function () {})).toEqual(n);
+      expect(this.network.on(null, function () {})).toEqual(this.network);
     });
 
     it("should forward on events to this.socket", function () {
       var handler = sinon.stub();
-      Network().on('event-name', handler);
+      this.network.on('event-name', handler);
       expect(this.socketon.firstCall.args[0]).toEqual('event-name');
       this.socketon.yield();
       expect(handler.called).toBeTruthy();
+    });
+
+    it("should not forward if socket is inactive", function () {
+      this.network.socket = undefined;
+      this.network.on('event-name', null);
+      expect(this.socketon.called).not.toBeTruthy();
     });
   });
 
   describe('.emit(event, data)', function () {
 
+    beforeEach(function () {
+      this.network = Network().connect('localhost');
+    });
+
     it("should forward on its arguments to this.socket.emit", function () {
       var event = 'event-name';
       var data = { health: 15 };
-      Network().emit(event, data);
+      this.network.emit(event, data);
       expect(this.socketemit.firstCall.calledWith(event, data)).toBeTruthy();
+    });
+
+    it("should not forward if socket is inactive", function () {
+      this.network.socket = undefined;
+      this.network.emit('event-name', null);
+      expect(this.socketemit.called).not.toBeTruthy();
     });
   });
 });
--- src/client/Network.js
+++ src/client/Network.js
@@ -2,28 +2,33 @@ var bind = require('lodash').bind;
 var io = require('socket.io-client');
 var Build = require('../util/Factory').Build;
 
-var Network = function () {
-  this.socket = io.connect(this.server, this.config);
-};
+var Network = function () {};
+
+Network.prototype.socket = undefined;
 
 Network.prototype.config = {
   transports: ['websocket'],
   'force new connection': true
 };
 
+Network.prototype.connect = function (server) {
+  this.socket = io.connect(server, this.config);
+  return this;
+};
+
 Network.prototype.on = function (event, handler) {
-  this.socket.on(event, bind(handler, this));
+  if (this.socket) {
+    this.socket.on(event, bind(handler, this));
+  }
   return this;
 };
 
 Network.prototype.emit = function (event, data) {
-  this.socket.emit(event, data);
+  if (this.socket) {
+    this.socket.emit(event, data);
+  }
 };
 
-var Factory = Build(Network, function (server) {
-  var opts = {};
-  opts.server = server;
-  return opts;
-});
+var Factory = Build(Network);
 
 module.exports = Factory;
