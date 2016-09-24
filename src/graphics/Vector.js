1098ce262571e033ca8e4f18702b5f25d7ed670c
Dan Brooks <danger.dan_@hotmail.co.uk>
2016-09-24 15:33:18 +0100

    Convert Vector over to ES6

--- src/graphics/Vector.js
+++ src/graphics/Vector.js
@@ -1,41 +1,25 @@
-var Build = require('../util/Factory').Build;
-var Point = require('./Point');
+'use strict';
 
-var Vector = function () {};
+let Point = require('./Point');
 
-Vector.prototype.from = Point(0, 0);
-Vector.prototype.to = Point(0, 0);
-
-/**
- * determine the current angle of the vector
- */
-Vector.prototype.angle = function () {
-  var dy = this.from.y - this.to.y;
-  var dx = this.from.x - this.to.x;
-  var theta = Math.atan2(dy, dx) * -1; // calculate angle in radians
-  theta *= 180 / Math.PI; // convert rads to degs, range
-  return (270 + theta) % 360;
-};
-
-/**
- * calculate the length of the vector
- */
-Vector.prototype.length = function () {
-  var dx = this.from.x - this.to.x;
-  var dy = this.from.y - this.to.y;
-  return Math.sqrt(dx * dx + dy * dy);
-};
-
-var Factory = Build(Vector, function (from, to) {
-
-  if (!Point.Created(from) || !Point.Created(to)) {
+module.exports = (from, to) => {
+  if (!Point.isValid(from) || !Point.isValid(to)) {
     throw new Error('Vector constructor takes two Point objects.');
   }
 
-  return {
-    from: from,
-    to: to
+  let angle = () => {
+    let dy = from.y - to.y;
+    let dx = from.x - to.x;
+    let theta = Math.atan2(dy, dx) * -1; // calculate angle in radians
+    theta *= 180 / Math.PI; // convert rads to degs, range
+    return (270 + theta) % 360;
   };
-});
 
-module.exports = Factory;
+  let length = () => {
+    let dx = from.x - to.x;
+    let dy = from.y - to.y;
+    return Math.sqrt(dx * dx + dy * dy);
+  };
+
+  return Object.freeze({ angle, length });
+};
