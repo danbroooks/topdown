2ba1a3e08d93c2f5c33334350dc8bc83b31490fe
Dan Brooks <danger.dan_@hotmail.co.uk>
2016-09-27 21:44:02 +0100

    collision refactor

--- src/graphics/Vector.js
+++ src/graphics/Vector.js
@@ -21,8 +21,6 @@ module.exports = (from, to) => {
     return Math.sqrt(d.x * d.x + d.y * d.y);
   };
 
-  let diff2 = () => ({ x: (to.x - from.x), y: (to.y - from.y) });
-
   let difference = (vector) => ({ x: from.x - vector.from.x, y: from.y - vector.from.y });
 
   let collision = (vector) => {
@@ -31,24 +29,47 @@ module.exports = (from, to) => {
       throw new Error('Invalid argument passed to vector.collision(), requires Vector');
     }
 
-    let s1_x = diff2().x;
-    let s1_y = diff2().y;
-    let s2_x = vector.diff2().x;
-    let s2_y = vector.diff2().y;
-    let divisor = (-s2_x * s1_y + s1_x * s2_y);
+    let a1 = from;
+    let a2 = to;
+    let b1 = vector.from;
+    let b2 = vector.to;
 
-    var s = (-s1_y * (difference(vector).x) + s1_x * (difference(vector).y)) / (divisor);
-    var t = (s2_x * (difference(vector).y) - s2_y * (difference(vector).x)) / (divisor);
+    var b2b1X = b2.x - b1.x;
+    var b2b1Y = b2.y - b1.y;
+    var a2a1X = a2.x - a1.x;
+    var a2a1Y = a2.y - a1.y;
+    var ab1X = a1.x - b1.x;
+    var ab1Y = a1.y - b1.y;
 
-    if (s >= 0 && s <= 1 && t >= 0 && t <= 1) {
-      return Point({
-        x: from.x + (t * s1_x),
-        y: from.y + (t * s1_y)
-      });
+    var u_b = b2b1Y * a2a1X - b2b1X * a2a1Y;
+    if (u_b == 0) {
+        if ((b2b1X * ab1Y - b2b1Y * ab1X) === 0 ||
+            (a2a1X * ab1Y - a2a1Y * ab1X) === 0) {
+
+            if (!(
+                a1.x < b1.x && a1.x < b2.x && a2.x < b1.x && a2.x < b2.x ||
+                a1.y < b1.y && a1.y < b2.y && a2.y < b1.y && a2.y < b2.y ||
+                a1.x > b1.x && a1.x > b2.x && a2.x > b1.x && a2.x > b2.x ||
+                a1.y > b1.y && a1.y > b2.y && a2.y > b1.y && a2.y > b2.y
+                )) {
+                return false;
+            }
+        }
+        return false;
+    }
+
+    var ua = (b2b1X * ab1Y - b2b1Y * ab1X) / u_b;
+    var ub = (a2a1X * ab1Y - a2a1Y * ab1X) / u_b;
+
+    if (0 <= ua && ua <= 1 && 0 <= ub && ub <= 1) {
+        return Point(
+          a1.x + ua * a2a1X,
+          a1.y + ua * a2a1Y
+        );
     }
 
     return false;
   };
 
-  return Object.freeze({ from, to, angle, length, collision, diff2 });
+  return Object.freeze({ from, to, angle, length, collision });
 };
